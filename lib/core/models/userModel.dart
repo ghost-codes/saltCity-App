6be8e7b8f50edad6d41b39ca5bf828a406ff3c87
Hope Dorkenoo <compounddork@gmail.com>
2020-12-27 13:45:06 +0000

    Api and Authentication Service Setup

--- lib/core/models/userModel.dart
+++ lib/core/models/userModel.dart
@@ -6,4 +6,13 @@ class UserModel {
   String mobile;
 
   UserModel({this.email, this.userName, this.uid, this.photoUrl});
+
+  factory UserModel.fromJson(json) {
+    return UserModel(
+      userName: json['userName'],
+      email: json['email'],
+      uid: json['uid'],
+      photoUrl: json['photoUrl'],
+    );
+  }
 }
--- lib/core/services/ApiService.dart
+++ lib/core/services/ApiService.dart
@@ -1,1 +1,14 @@
-class Api {}
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:saltcity_app/core/models/userModel.dart';
+
+class Api {
+  static const endpoint = "";
+
+  var client = http.Client();
+
+  Future<UserModel> signUp(Map<String, String> signUpCredentials) async {
+    Response response =
+        await client.post('$endpoint/user/signUp', body: signUpCredentials);
+  }
+}
